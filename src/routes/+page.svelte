dabee5706a38518b726c61bf8255826bde9a11ad
mouse_484 <escmouse484@gmail.com>
2023-02-08 05:09:43 +0000

    リレーに接続してグローバルを表示

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { relayInit, nip19 } from 'nostr-tools';
+	import { onMount } from 'svelte';
+
+	let status = { relay: { connect: false } };
+
+	let posts: HTMLDivElement;
+
+	onMount(async () => {
+		const relay = relayInit('wss://relay.damus.io');
+		await relay.connect();
+
+		relay.on('connect', () => (status.relay.connect = true));
+
+		const sub = relay.sub([
+			{
+				since: Math.floor(new Date().getTime() / 1000),
+				// authors: [''],
+				kinds: [1]
+			}
+		]);
+
+		sub.on('event', (event: { content: string }) => {
+			const child = document.createElement('span');
+			child.classList.add('post');
+			child.textContent = event.content;
+			posts.appendChild(child);
+		});
+	});
+</script>
+
+<div>
+	リレー: {status.relay.connect && '接続'}
+</div>
+
+<div class="posts" bind:this={posts} />
+
+<style>
+	.posts {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+	}
+</style>
