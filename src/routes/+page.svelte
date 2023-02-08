068a044e62bce53e3330bfc5d840459046dc505d
mouse_484 <escmouse484@gmail.com>
2023-02-08 07:43:40 +0000

    複数リレー

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,44 @@
 <script lang="ts">
-	import { relayInit, nip19 } from 'nostr-tools';
+	import { RelayPool } from 'nostr-relaypool';
 	import { onMount } from 'svelte';
+	import { nip19 } from 'nostr-tools';
+
+	const npubToHexId = (npub: string) => {
+		const { data: userId } = nip19.decode(npub);
+		return userId as string;
+	};
 
 	let status = { relay: { connect: false } };
 
 	let posts: HTMLDivElement;
 
+	const relays = ['wss://relay.damus.io'];
+
 	onMount(async () => {
-		const relay = relayInit('wss://relay.damus.io');
-		await relay.connect();
+		const relayPool = new RelayPool(relays);
 
-		relay.on('connect', () => (status.relay.connect = true));
-
-		const sub = relay.sub([
-			{
-				since: Math.floor(new Date().getTime() / 1000),
-				// authors: [''],
-				kinds: [1]
-			}
-		]);
-
-		sub.on('event', (event: { content: string }) => {
-			const child = document.createElement('span');
-			child.classList.add('post');
-			child.textContent = event.content;
-			posts.appendChild(child);
-		});
+		relayPool.subscribe(
+			[
+				{
+					authors: [npubToHexId('npub1ecxns5jjwvaasnq7nnna0nd4wvacqgdmpvm5pjzdrpzcp06q863s0w23y6')],
+					kinds: [1]
+				}
+			],
+			relays,
+			(event, isAfterEose, relayURL) => {
+				console.log(event, isAfterEose, relayURL);
+				const child = document.createElement('span');
+				child.classList.add('post');
+				child.textContent = event.content;
+				posts.appendChild(child);
+			},
+			undefined
+		);
 	});
 </script>
 
 <div>
-	リレー: {status.relay.connect && '接続'}
+	リレー: {relays}
 </div>
 
 <div class="posts" bind:this={posts} />
