73852eb44799dc921e3a6478b763559637792bec
mouse_484 <escmouse484@gmail.com>
2023-02-09 12:23:13 +0000

    重複チェックと並び替え

--- src/lib/components/TimeLine.svelte
+++ src/lib/components/TimeLine.svelte
@@ -6,16 +6,23 @@
 	export let pubkey: string;
 	export let relayPool: Relay[];
 
-	let notes: relayEvent[] = [];
+	let notes: relayEvent[];
 
 	relayPool.forEach((relay) => {
 		const sub = relay.sub([{ authors: [npubToHexId(pubkey)], kinds: [1] }]);
 		sub.on('event', (event: relayEvent) => {
 			notes = [...notes, event];
 		});
+		sub.on('eose', () => {
+			sub.unsub();
+			// 完全に重複しないと弾けないけどリレーごとの挙動きになるしこのままで（とりあえず）
+			notes = [...new Set(notes)].sort((a, b) => b.created_at - a.created_at);
+		});
 	});
 </script>
 
-{#each notes as note}
-	<Note {note} />
-{/each}
+{#if notes}
+	{#each notes as note}
+		<Note {note} />
+	{/each}
+{/if}
