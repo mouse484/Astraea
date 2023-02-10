ca346093ef09b6669d4c9591f33acec51a3cf9b9
mouse_484 <escmouse484@gmail.com>
2023-02-10 02:51:27 +0000

    root width 100%

---
+++ src/lib/styles/App.css.ts
@@ -0,0 +1,5 @@
+import { style } from '@vanilla-extract/css';
+
+export const appClass = style({
+	width: '100%'
+});
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Relay from '$lib/components/Relay.svelte';
+	import { appClass } from '$lib/styles/App.css';
 </script>
 
-<Relay />
-
-<slot />
+<div class={appClass}>
+	<Relay />
+	<slot />
+</div>
