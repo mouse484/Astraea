f9ed359975caf21a474917b37c21f3c84a539dd1
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-02-22 12:59:27 +0000

    ちゃんとリンクと#系が共存できるように

--- src/lib/utils/formatContent.ts
+++ src/lib/utils/formatContent.ts
@@ -9,7 +9,7 @@ export const formatContent = (event: Event) => {
 	const sanitized = sanitize(content);
 	const cc = sanitized.replaceAll(/#\[([0-9])\]/g, (_all, $1) => {
 		const [type, id] = tags[$1] || []; // 仮
-		return ` ${type === 'p' ? `#[${id}]` : ""} `;
+		return `${type === 'p' ? `#[${id}]` : ''}`;
 	});
 	const linked = linkifyHtml(cc, {
 		nl2br: true,
@@ -37,8 +37,5 @@ export const formatContent = (event: Event) => {
 		}
 	});
 
-	return sanitize(linked, {
-		ALLOWED_TAGS: ['svelte:self'],
-		ALLOWED_ATTR: ['noteId']
-	});
+	return sanitize(linked);
 };
