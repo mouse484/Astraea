45e38e5444c0055fb3e20ce04f8eb6dfb1da87f1
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-06 07:30:33 +0000

    リレープールを関数化

---
+++ src/lib/utils/relay.ts
@@ -0,0 +1,41 @@
+import { SimplePool, type Event, type Filter } from 'nostr-tools';
+
+const defaultRelays = [
+	'wss://relay.damus.io',
+	'wss://relay.snort.social',
+	'wss://eden.nostr.land'
+];
+
+// relay取得する仕組みあとで
+
+class RelayEventer<T extends unknown[]> {
+	private event!: (...args: T) => void;
+	on(callback: (...args: T) => void) {
+		this.event = callback;
+	}
+	emit(...args: T) {
+		this.event?.(...args);
+	}
+}
+
+class RelayPool {
+	pool: SimplePool;
+	relays: string[] = defaultRelays;
+	constructor() {
+		this.pool = new SimplePool();
+	}
+	subscribe(kind: number, filter: Filter = {}) {
+		const subEE = new RelayEventer<[Event]>();
+		console.log({ kinds: [kind], ...filter });
+		const subs = this.pool.sub(this.relays, [{ kinds: [kind], ...filter }]);
+
+		subs.on('event', (event: Event) => {
+			subEE.emit(event);
+		});
+		subs.on('eose', () => subs.unsub());
+
+		return subEE;
+	}
+}
+
+export const relayPool = new RelayPool();
