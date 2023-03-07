a6b3a2e73b02b0c340718026de55512b253643f8
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-07 13:05:20 +0000

    reset all

--- src/lib/styles/App.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@vanilla-extract/css';
-import { sprinkles } from './sprinkles.css';
-
-export const appClass = style([
-	{
-		maxWidth: '100%'
-	},
-	sprinkles({ padding: 'small' })
-]);
--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Header from '$lib/components/header/Header.svelte';
-	import { appClass } from '$lib/styles/App.css';
-</script>
-
-<div class={appClass}>
-	<Header />
-	<slot />
-</div>
