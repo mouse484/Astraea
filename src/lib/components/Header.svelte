28fba77f7bdb4c6bf2d0dbe8edacf1f2ebbec674
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-07 13:18:08 +0000

    新Headerベース

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { myNpubHex } from '$lib/data/const';
+	import Profile from './Profile.svelte';
+</script>
+
+<div>
+	<h1>Nostr Client</h1>
+
+	<Profile npubHex={myNpubHex} imageOnly={true} />
+</div>
---
+++ src/lib/components/Profile.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export let npubHex: string;
+	export let imageOnly = false;
+</script>
+
+{npubHex}
