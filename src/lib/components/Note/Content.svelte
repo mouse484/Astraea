8ea990caffef109f667340758fa210c5c855f9e2
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-08 10:55:50 +0000

    Markdownitのrenderをやめてsvelteで書く

--- src/lib/components/Note/Content.svelte
+++ src/lib/components/Note/Content.svelte
@@ -1,42 +1,53 @@
 <script lang="ts">
 	import DOMPurify from 'isomorphic-dompurify';
-	import MarkdownIt, { type PluginSimple } from 'markdown-it';
-	import Token from 'markdown-it/lib/token';
+	import MarkdownIt from 'markdown-it';
 
 	const { sanitize } = DOMPurify;
 
 	export let rawContent: string;
 
-	const imageLinkToImgTag: PluginSimple = (md) => {
-		md.core.ruler.after('linkify', 'image2link', (state) => {
-			state.tokens = state.tokens.flatMap((token) => {
-				if (token.type !== 'inline') return token;
-				if (!token.children) return token;
-				return token.children.map((child) => {
-					if (child.type === 'link_open') {
-						const href = child.attrGet('href');
-						if (href?.match(/(jpe?g|png|gif|webp)$/)) {
-							const imgToken = new Token('img', 'img', 0);
-							imgToken.attrSet('src', href);
-							imgToken.attrSet('class', 'h-48 border-2 border-indigo-200 p-1 rounded');
-							return imgToken;
-						}
-					}
-					return child;
-				});
-			});
-			return state;
-		});
-	};
-
 	const md = MarkdownIt({ linkify: true, breaks: true });
-	md.use(imageLinkToImgTag);
 
 	const sanitized = sanitize(rawContent);
 
-	const result = md.render(sanitized);
+	const parsed = md.parse(sanitized, {});
+
+	// index, tag, href/src
+	let next: [number | undefined, string, string] = [undefined, '', ''];
+
+	const linkStart = (index: number, href: string) => {
+		const isImage = href?.match(/(jpe?g|png|gif|webp)$/);
+		next = [(index += 1), isImage ? 'img' : 'a', href];
+		return '';
+	};
 </script>
 
 <div class="break-words">
-	{@html result}
+	{#each parsed as token}
+		{#if token.type === 'inline'}
+			{#each token.children || [] as child, index}
+				{@const content = child.content}
+				{#if child.type === 'text'}
+					{@const [i, tag, href] = next}
+					{#if i === index}
+						{#if tag === 'a'}
+							<a {href}>{content}</a>
+						{:else if tag === 'img'}
+							<img
+								class="h-48 border-2 border-indigo-200 p-1 rounded"
+								src={href}
+								alt={content}
+							/>
+						{/if}
+					{:else}
+						<p>{content}</p>
+					{/if}
+				{:else if child.tag === 'br'}
+					<br />
+				{:else if child.type === 'link_open'}
+					{linkStart(index, child.attrGet('href') || '')}
+				{/if}
+			{/each}
+		{/if}
+	{/each}
 </div>
