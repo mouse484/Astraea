d1a4c2fbe0dedec265cb0f3951e90a273c2b5edb
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-08 01:40:27 +0000

    プロフィールを表示

--- src/lib/components/Note.svelte
+++ src/lib/components/Note.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import type { Event } from 'nostr-tools';
+	import Profile from './Profile.svelte';
 
 	export let note: Event;
 </script>
 
 <div>
-	{note.content}
+	<Profile npubHex={note.pubkey} />
+	<div>
+		{note.content}
+	</div>
 </div>
--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { get } from '$lib/utils/nostr';
-	import { onMount } from 'svelte';
 
 	export let npubHex: string;
 	export let imageOnly = false;
@@ -9,14 +8,18 @@
 {#await get({ kinds: [0], authors: [npubHex] }) then event}
 	{#if event}
 		{@const profile = JSON.parse(event.content)}
-		<img src={profile?.picture} alt={profile?.name} />
-		{#if !imageOnly}
-			<div>
-				<a href="後で">
-					{profile?.display_name || profile?.name}
-				</a>
-				<div>{profile?.nip05 || `@${profile?.name}`}</div>
-			</div>
-		{/if}
+		<div class="flex">
+			<img class="h-12 rounded" src={profile?.picture} alt={profile?.name} />
+			{#if !imageOnly}
+				<div class="pl-4">
+					<a href="#後で">
+						{profile?.display_name || profile?.name}
+					</a>
+					<div class="text-xs text-gray-500">
+						{profile?.nip05 || `@${profile?.name}`}
+					</div>
+				</div>
+			{/if}
+		</div>
 	{/if}
 {/await}
