4f89ef24113eec78f9000185f2ab8f63f88b1618
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-08 02:52:10 +0000

    リプライの表示

--- src/lib/components/Note/Note.svelte
+++ src/lib/components/Note/Note.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
+	import { get } from '$lib/utils/nostr';
 	import type { Event } from 'nostr-tools';
 	import Profile from '../Profile.svelte';
 	import Content from './Content.svelte';
 
 	export let note: Event;
+	export let isReplay = false;
 </script>
 
-<div>
+{#if !isReplay}
+	{#each note.tags as a, index}
+		{@const [type, id, , marker] = a}
+		{#if type === 'e' && ((!marker && index >= 1) || marker === 'reply')}
+			{#await get({ kinds: [1], ids: [id] }) then event}
+				<svelte:self note={event} isReplay={true} />
+			{/await}
+		{/if}
+	{/each}
+{/if}
+<div class={`border p-4 rounded ${isReplay ? 'mb-0 border-b-0' : 'mb-4'}`}>
 	{#if note.pubkey}
 		<Profile npubHex={note.pubkey} />
 	{/if}
-	<div>
+	<div class="mt-4">
 		<Content rawContent={note.content} />
 	</div>
 </div>
