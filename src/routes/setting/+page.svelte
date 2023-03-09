4d36a5a110c26b0d61c4741e689d2fbda88fc991
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-09 11:41:24 +0000

    pubkeyの保存にメッセージを出す

--- src/routes/setting/+page.svelte
+++ src/routes/setting/+page.svelte
@@ -2,13 +2,20 @@
 	import { pubkey, relays } from '$lib/data/setting';
 	import { nip19 } from 'nostr-tools';
 
-	let inputPubkey: string = $pubkey;
+	let inputPubkey: string;
+	let pubkeyInfo: string;
 
 	const savePubkey = () => {
-		const decode = nip19.decode(inputPubkey);
-		if (decode.type === 'npub') {
-			// 後で直す
-			pubkey.set(decode.data as string);
+		try {
+			const decode = nip19.decode(inputPubkey);
+			if (decode.type === 'npub') {
+				// 後で直す
+				pubkey.set(decode.data as string);
+				pubkeyInfo =
+					'pubkeyを保存しました、プロフィールリンクから確認できます。';
+			}
+		} catch {
+			pubkeyInfo = 'pubkeyが間違っているか、問題が発生しました。';
 		}
 	};
 </script>
@@ -27,10 +34,20 @@
 			bind:value={inputPubkey}
 			placeholder="npub key"
 		/>
-		<button class="px-2 ml-2 rounded border bg-slate-200" on:click={savePubkey}>
+		<button
+			class="px-2 ml-2 rounded border bg-slate-200"
+			on:click={savePubkey}
+			disabled={!inputPubkey}
+		>
 			save
 		</button>
 	</div>
+
+	{#key pubkeyInfo}
+		{#if pubkeyInfo}
+			<p>{pubkeyInfo}</p>
+		{/if}
+	{/key}
 </div>
 
 <h3 class="mt-8">Relays</h3>
