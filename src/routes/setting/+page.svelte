87868fa41561212d74f71aa8cdb8d5b0b565e3ff
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-09 01:15:29 +0000

    設定ページから自分のプロフィールにいけるように
    ついでに見た目も調整

--- src/routes/setting/+page.svelte
+++ src/routes/setting/+page.svelte
@@ -2,7 +2,7 @@
 	import { pubkey, relays } from '$lib/data/setting';
 	import { nip19 } from 'nostr-tools';
 
-	let inputPubkey: string;
+	let inputPubkey: string = $pubkey;
 
 	const savePubkey = () => {
 		const decode = nip19.decode(inputPubkey);
@@ -13,10 +13,27 @@
 	};
 </script>
 
-<input type="text" bind:value={inputPubkey} placeholder="npub key" />
-<button on:click={savePubkey}>save</button>
+<div>
+	<a href="/profile/{$pubkey}">Go Profile</a>
+</div>
 
-<h3>Relays</h3>
+<div class="mt-4">
+	<label for="npub">Npub key: </label>
+
+	<div class="flex">
+		<input
+			class="mt-2 w-full rounded border border-black"
+			type="text"
+			bind:value={inputPubkey}
+			placeholder="npub key"
+		/>
+		<button class="px-2 ml-2 rounded border bg-slate-200" on:click={savePubkey}>
+			save
+		</button>
+	</div>
+</div>
+
+<h3 class="mt-8">Relays</h3>
 {#each $relays as relay}
 	<p>{relay}</p>
 {/each}
