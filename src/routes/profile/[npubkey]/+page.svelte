b453fb7bdc7623cd81f1f306b3c347cc654f60b7
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-09 00:40:05 +0000

    プロフィールページのベース、鍵を渡す

---
+++ src/routes/profile/[npubkey]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import type { response } from './+page';
+
+	export let data: PageData;
+
+	let { hexId } = data as response;
+</script>
+
+{hexId}
