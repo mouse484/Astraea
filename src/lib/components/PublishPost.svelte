6ab3b249bde8b3664b24a263989b6c5bf34a06b6
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-12 04:33:26 +0000

    fix: ctrl+enterで送信

--- src/lib/components/PublishPost.svelte
+++ src/lib/components/PublishPost.svelte
@@ -17,6 +17,12 @@
 
 	$: isPublish = !(isNip07 && !!content);
 
+	const keydown = (event: KeyboardEvent) => {
+		if (event.ctrlKey && event.key === 'Enter') {
+			publishPost();
+		}
+	};
+
 	const publishPost = async () => {
 		if (!window.nostr) return;
 		const unsignedEvent: UnsignedEvent = {
@@ -48,6 +54,7 @@
 		<textarea
 			class="p-2 pr-24 w-full h-32 rounded border resize-none"
 			bind:value={content}
+			on:keydown={keydown}
 		/>
 		{#key isPublish}
 			<button
