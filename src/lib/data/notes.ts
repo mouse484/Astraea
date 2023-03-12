dc004bad31f9d8c39f2eac06b9253da2f0fabfe6
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-12 14:10:34 +0000

    feat: notesの更新をしやすく

--- src/lib/data/notes.ts
+++ src/lib/data/notes.ts
@@ -1,6 +1,36 @@
 import type { Event } from 'nostr-tools';
 import { writable } from 'svelte/store';
 
+export const notesUpdater = (
+	id: string,
+	event: Event,
+	append: 'root' | 'reply' = 'root'
+) => {
+	notes.update((i) => {
+		const current = i.get(event.id);
+		const eventTime = new Date(event.created_at * 1000);
+		const updated = current?.updated
+			? current.updated > eventTime
+				? current.updated
+				: eventTime
+			: eventTime;
+
+		const value = {
+			...current,
+			updated
+		};
+
+		switch (append) {
+			case 'root':
+				value.root = event;
+				break;
+			case 'reply':
+				value.reply = (current?.reply || new Map()).set(event.id, event);
+		}
+		return i.set(id, value);
+	});
+};
+
 export const notes = writable(
 	new Map<string, { updated: Date; root?: Event; reply?: Map<string, Event> }>()
 );
