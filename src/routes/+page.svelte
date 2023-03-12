e87ae7a89479a29ba4a686351defc42c82897852
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-12 13:50:43 +0000

    feat: kind1 Event周りの改善

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,20 +2,23 @@
 	import PublishPost from '$lib/components/PublishPost.svelte';
 	import TimeLine from '$lib/components/TimeLine.svelte';
 	import Trend from '$lib/components/Trend.svelte';
+	import { contacts } from '$lib/data/contacts';
 	import { pubkey } from '$lib/data/setting';
 	import { get } from '$lib/utils/nostr';
 
 	const getContracts = async () => {
-		const contacts = new Set<string>();
-
+		if ($contacts.size) return [...$contacts];
 		const contactsEvent = await get({ kinds: [3], authors: [$pubkey] });
 		if (!contactsEvent) return;
 		const tags = contactsEvent.tags as [string, string][];
-		tags.forEach(([, id]) => {
-			contacts.add(id);
-		});
-
-		return [...contacts];
+		contacts.set(
+			new Set(
+				tags.map(([, id]) => {
+					return id;
+				})
+			)
+		);
+		return [...$contacts];
 	};
 </script>
 
