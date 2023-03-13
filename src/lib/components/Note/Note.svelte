667fe68bc7f45520ae11c02fa0bef062ed5bf9aa
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-13 12:36:26 +0000

    fix: noteへのlink

--- src/lib/components/Note/Note.svelte
+++ src/lib/components/Note/Note.svelte
@@ -8,32 +8,23 @@
 
 	export let note: Event;
 	export let isReplay = false;
-
-	const noteClick = () => {
-		const selection = window.getSelection()?.toString();
-		if (!selection) {
-			goto(`/note/${note.id}`);
-		}
-	};
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<div
-	class={`border p-4 rounded ${isReplay ? 'mt-0 border-t-0' : 'mt-4'}`}
-	on:click={noteClick}
->
-	{#if note.pubkey}
-		<Profile npubHex={note.pubkey} />
-	{/if}
-	<div class="mt-4">
-		<Content rawContent={note.content} event={note} />
-	</div>
-	<div class="mt-4 flex justify-between">
-		<NoteFooter event={note} />
-		{#if note.created_at}
-			<div class="text-sm text-slate-600">
-				{new Date(note.created_at * 1000).toLocaleString()}
-			</div>
+<a href={`/note/${note.id}`}>
+	<div class={`border p-4 rounded ${isReplay ? 'mt-0 border-t-0' : 'mt-4'}`}>
+		{#if note.pubkey}
+			<Profile npubHex={note.pubkey} />
 		{/if}
+		<div class="mt-4">
+			<Content rawContent={note.content} event={note} />
+		</div>
+		<div class="mt-4 flex justify-between">
+			<NoteFooter event={note} />
+			{#if note.created_at}
+				<div class="text-sm text-slate-600">
+					{new Date(note.created_at * 1000).toLocaleString()}
+				</div>
+			{/if}
+		</div>
 	</div>
-</div>
+</a>
