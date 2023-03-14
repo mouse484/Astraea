d7f9e145014efa12d7e0ed8784b13a48df0db957
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-14 09:59:11 +0000

    feat: コピーした情報を見えるように

--- src/lib/components/CopyButton.svelte
+++ src/lib/components/CopyButton.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
+	import { writable } from 'svelte/store';
 	import Button from './elements/Button.svelte';
 
 	export let content: string;
+	const copied = writable(false);
 
 	const onClick = async () => {
 		await navigator.clipboard.writeText(content);
+		copied.set(true);
 	};
+
+	copied.subscribe((value) => {
+		if (value) {
+			setTimeout(() => copied.set(false), 1000);
+		}
+	});
 </script>
 
-<Button on:click={onClick}>
-	<slot />
+<Button on:click={onClick} disabled={$copied}>
+	{#if $copied}
+		コピーしました
+	{:else}
+		<slot />
+	{/if}
 </Button>
--- src/lib/components/Profile/ProfileMenu.svelte
+++ src/lib/components/Profile/ProfileMenu.svelte
@@ -16,7 +16,7 @@
 	</button>
 	{#if isMenuOpen}
 		<div
-			class="flex absolute right-0 z-50 flex-col p-2 w-max rounded border bg-inherit"
+			class="flex absolute right-0 z-50 flex-col gap-2 p-4 mt-4 w-max bg-white rounded border"
 		>
 			<CopyButton content={npubHex}>ユーザーIDをコピー(pubkey)</CopyButton>
 			<CopyButton content={npub}>ユーザーIDをコピー(hex)</CopyButton>
