58acd6c7df33dd2284ddd52b73b1aacde2477967
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-14 09:49:01 +0000

    feat: npubとhexのコピー機能
    close #164

--- src/lib/components/Profile/ProfileMenu.svelte
+++ src/lib/components/Profile/ProfileMenu.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import { nip19 } from 'nostr-tools';
 	import CopyButton from '../CopyButton.svelte';
 
+	export let npubHex: string;
+
 	let isMenuOpen = false;
+
+	$: npub = nip19.npubEncode(npubHex);
 </script>
 
 <div class="relative">
@@ -10,9 +15,11 @@
 		<Icon icon="mdi:menu" />
 	</button>
 	{#if isMenuOpen}
-		<div class="absolute right-0 z-50 p-2 rounded border bg-inherit">
-			<!-- <CopyButton>ユーザーIDをコピー(pubkey)</CopyButton> -->
-			<!-- <CopyButton>ユーザーIDをコピー(hex)</CopyButton> -->
+		<div
+			class="flex absolute right-0 z-50 flex-col p-2 w-max rounded border bg-inherit"
+		>
+			<CopyButton content={npubHex}>ユーザーIDをコピー(pubkey)</CopyButton>
+			<CopyButton content={npub}>ユーザーIDをコピー(hex)</CopyButton>
 		</div>
 	{/if}
 </div>
