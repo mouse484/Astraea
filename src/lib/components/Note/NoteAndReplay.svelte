f4cf4a003487271bdbfe94c11337d4324e650948
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-15 01:17:36 +0000

    feat: いいねされた通知ページを作成
    リポストはまだzapは未定

--- src/lib/components/Note/NoteAndReplay.svelte
+++ src/lib/components/Note/NoteAndReplay.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { getNote } from '$lib/data/notes';
 	import type { Event } from 'nostr-tools';
+	import AwaitNote from './AwaitNote.svelte';
 	import Note from './Note.svelte';
 
 	export let id: string;
@@ -11,15 +11,7 @@
 {#if root}
 	<Note note={root} />
 {:else}
-	{#await getNote(id)}
-		<p>Loading...</p>
-	{:then event}
-		<Note note={event} />
-	{:catch}
-		<a class="text-blue-600 visited:text-purple-600" href="/note/{id}">
-			note:{id.substring(0, 8)}..
-		</a>
-	{/await}
+	<AwaitNote {id} />
 {/if}
 {#if reply}
 	{#each reply as event}
