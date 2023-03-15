cfe5dd2e47e53ea3e1894ea9960ea549b1b774a2
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-15 01:37:43 +0000

    feat: ヘッダーに通知ボタンを作成

---
+++ src/lib/components/Notifications/NotificationHeader.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import Notifications from './Notifications.svelte';
+
+	let isNoticeOpen = false;
+</script>
+
+<div class="relative">
+	<button on:click={() => (isNoticeOpen = !isNoticeOpen)}>
+		{#if isNoticeOpen}
+			<Icon icon={'mdi:bell'} />
+		{:else}
+			<Icon icon={'mdi:bell-outline'} />
+		{/if}
+	</button>
+	{#if isNoticeOpen}
+		<div class="absolute right-0 z-50 p-4 mt-4 w-96 bg-white rounded border">
+			<Notifications />
+		</div>
+	{/if}
+</div>
