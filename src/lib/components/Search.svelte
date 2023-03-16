0dc8d74e98600acad29057fae6170cc7f33f87de
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-16 06:53:32 +0000

    feat: 検索BOXをコンポーネント化

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Button from './elements/Button.svelte';
+
+	let searchInput: string = '';
+</script>
+
+<form class="flex gap-4 w-full" action="/search">
+	<input
+		class="p-2 border rounded grow"
+		type="text"
+		name="q"
+		value={searchInput}
+	/>
+	<Button type="submit">検索</Button>
+</form>
