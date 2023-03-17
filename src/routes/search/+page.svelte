62b452e9bb1325eaded3b1285c659250f6e7bd8a
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-17 14:29:34 +0000

    fix: 検索が適切に行われるようにする

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,57 +1,59 @@
 <script lang="ts">
-	import { afterNavigate, goto } from '$app/navigation';
+	import { afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Loading from '$lib/components/elements/Loading.svelte';
 	import Note from '$lib/components/Note/Note.svelte';
 	import Search from '$lib/components/Search.svelte';
 	import { subscribeEvents } from '$lib/utils/nostr';
-	import type { Event, Filter } from 'nostr-tools';
+	import type { Event, Filter, Sub } from 'nostr-tools';
 	import { onDestroy } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	const searchQuery = writable('');
 
 	let notes: Event[] = [];
-	let searchQuery: string | undefined = undefined;
+	let sub: Sub | undefined;
 	let end = false;
 	let endtimer: NodeJS.Timeout;
 
 	afterNavigate(() => {
-		searchQuery = $page.url.searchParams.get('q') || '';
-		if (!searchQuery) return;
+		searchQuery.set($page.url.searchParams.get('q') || '');
+	});
+
+	const unsubscribe = searchQuery.subscribe((query) => {
+		let length = notes.length;
+
+		sub?.unsub();
+		clearInterval(endtimer);
 		notes = [];
-		end = false;
-		const sub = subscribeEvents(
-			{ kinds: [1], search: searchQuery, limit: 30 } as Filter,
-			['wss://relay.nostr.band']
-		);
+
+		if (!query) return;
+
+		sub = subscribeEvents({ kinds: [1], search: query, limit: 30 } as Filter, [
+			'wss://relay.nostr.band'
+		]);
 		sub.on('event', (event: Event) => {
 			notes = [...notes, event];
-		});
-		sub.on('eose', () => {
-			let length = notes.length;
-			const endtimer = setInterval(() => {
-				if (length === notes.length) {
-					end = true;
-					sub.unsub();
-					clearInterval(endtimer);
-				} else {
-					length = notes.length;
-				}
-			}, 5000);
 		});
 	});
 
 	onDestroy(() => {
+		sub?.unsub();
 		clearInterval(endtimer);
+		notes = [];
+		unsubscribe();
 	});
 </script>
 
 <h2 class="text-2xl">検索</h2>
 
 <div class="mt-8 flex w-full">
-	<Search bind:searchInput={searchQuery} />
+	<Search />
 </div>
 
 <section class="mt-8">
-	{#if searchQuery !== undefined}
+	<h3 class="text-2xl">{$searchQuery}</h3>
+	{#if $searchQuery}
 		{#key notes}
 			{#if notes.length}
 				{#each notes as note}
@@ -61,14 +63,8 @@
 				<Loading>Notes</Loading>
 			{/if}
 		{/key}
-		{#if end}
-			<p class="mt-8 text-right">
-				{#if notes.length}
-					{notes.length}/30(max)
-				{:else}
-					見つかりませんでした
-				{/if}
-			</p>
-		{/if}
+		<p class="mt-8 text-right">
+			{notes.length || 0}/30(max)
+		</p>
 	{/if}
 </section>
