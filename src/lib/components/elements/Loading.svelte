f65f78ce967328f0064f5d5fadd37fb398bee84c
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-18 23:48:24 +0000

    BREAKING CHANGE: 書き直しわかりやすくするために一回全部消す

--- src/lib/components/elements/Loading.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import Icon from '@iconify/svelte';
-</script>
-
-<div class="flex flex-col items-center w-full">
-	<Icon icon={'mdi:loading'} width={32} class="animate-spin" />
-	<p class="mt-8">Loading <slot />...</p>
-</div>
--- src/lib/data/const.ts
+++
@@ -1,8 +0,0 @@
-export const myNpubHex =
-	'ce0d385252733bd84c1e9ce7d7cdb5733b8021bb0b3740c84d184580bf403ea3';
-
-export const defaultRelays = [
-	'wss://relay.damus.io',
-	'wss://relay.snort.social',
-	'wss://eden.nostr.land'
-];
