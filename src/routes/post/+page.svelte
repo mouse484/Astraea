dfd4c7c61a06a7e91a88d62b4f49c82542e254f0
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-18 23:48:24 +0000

    BREAKING CHANGE: 書き直しわかりやすくするために一回全部消す

--- src/routes/post/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import PublishPost from '$lib/components/PublishPost.svelte';
-</script>
-
-<PublishPost />
--- src/routes/trend/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import Trend from '$lib/components/Trend.svelte';
-</script>
-
-<Trend />
