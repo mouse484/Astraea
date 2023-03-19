34e4cc52ebc1896ba43d0807eab2a7eaa216a808
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-19 03:54:37 +0000

    feat: relayの詳細表示

--- src/routes/setting/+page.svelte
+++ src/routes/setting/+page.svelte
@@ -3,12 +3,35 @@
 	import Button from '$lib/components/elements/Button.svelte';
 	import Heading from '$lib/components/elements/Heading.svelte';
 	import Section from '$lib/components/elements/Section.svelte';
-	import { pubkeyClear } from '$lib/store/setting';
+	import RelayInfo from '$lib/components/RelayInfo.svelte';
+	import { pubkeyClear, relays } from '$lib/store/setting';
 </script>
 
 <Heading>設定</Heading>
 
 <Section>
+	<h3>Relays</h3>
+	<table class="table-auto border-spacing-4">
+		<thead>
+			<tr>
+				<th>url</th>
+				<th>read</th>
+				<th>write</th>
+				<th>info</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each Object.entries($relays) as [relay, { read, write }]}
+				<tr>
+					<td>{relay}</td>
+					<td>{read}</td>
+					<td>{write}</td>
+					<td> <RelayInfo relayUrl={relay} /></td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+
 	<h3>ログアウト</h3>
 	<Button
 		on:click={() => {
