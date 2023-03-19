f9066fd93e77449fc297316c5034b4987a7d77a0
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-19 00:33:03 +0000

    feat: relayの取得 read write対応

--- src/lib/store/setting.ts
+++ src/lib/store/setting.ts
@@ -1,16 +1,41 @@
 import { dev } from '$app/environment';
-import { mouseNpubKey } from '$lib/data/const';
+import { defaultRelays, mouseNpubKey } from '$lib/data/const';
+import { subscribeEvents } from '$lib/utils/nostr';
 import { writable } from 'svelte/store';
+import { z } from 'zod';
 
 export const pubkey = writable<string>(dev ? mouseNpubKey : '');
+export const relays = writable<
+	Record<
+		string,
+		{
+			read: boolean;
+			write: boolean;
+		}
+	>
+>(defaultRelays);
 
 export const pubkeyClear = () => {
 	pubkey.set('');
 	localStorage.removeItem('pubkey');
 };
 
+const relayScheme = z.record(
+	z.object({
+		read: z.boolean(),
+		write: z.boolean()
+	})
+);
+
 pubkey.subscribe(async (newKey) => {
 	if (import.meta.env.SSR) return;
 	if (!newKey) return;
 	localStorage.setItem('pubkey', newKey);
+	const sub = subscribeEvents(3, { authors: [newKey] });
+	sub.on('event', (event) => {
+		const parsed = relayScheme.parse(event.content);
+		if (parsed) {
+			relays.set(parsed);
+		}
+	});
 });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	import Heading from '$lib/components/elements/Heading.svelte';
+	import Section from '$lib/components/elements/Section.svelte';
+	import TimeLine from '$lib/components/TimeLine.svelte';
 </script>
 
 <Heading>ホーム</Heading>
+
+<Section>
+	<TimeLine />
+</Section>
