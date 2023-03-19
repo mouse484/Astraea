a0c9b3d81d0dc273b3481aeefe7164efea6840ab
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-19 03:31:32 +0000

    feat: リプライを表示

--- src/lib/components/Note/Note.svelte
+++ src/lib/components/Note/Note.svelte
@@ -3,21 +3,32 @@
 	import Profile from '../Profile/Profile.svelte';
 	import Content from './Content.svelte';
 	import Footer from './Footer.svelte';
+	import NoteWithId from './NoteWithId.svelte';
 
 	export let event: Event;
+
+	const isReply = event.tags.reverse().find(([type, , , marker]) => {
+		return type === 'e' && (!marker || marker === 'reply');
+	});
 </script>
 
-{#if event}
-	<div class="border p-2 m-2 rounded">
-		<Profile pubkey={event.pubkey} detail={false} />
-		<div class="mt-4">
-			<Content rawContent={event.content} tags={event.tags} />
-		</div>
-		<div class="mt-4 flex justify-between">
-			<Footer {event} />
-			<div>
-				{new Date(event.created_at * 1000).toLocaleString()}
+<div>
+	{#if event}
+		{#if isReply}
+			{@const [, id] = isReply}
+			<NoteWithId {id} />
+		{/if}
+		<div class="border p-2 rounded {isReply ? 'border-t-0' : ''}">
+			<Profile pubkey={event.pubkey} detail={false} />
+			<div class="mt-4">
+				<Content rawContent={event.content} tags={event.tags} />
+			</div>
+			<div class="mt-4 flex justify-between">
+				<Footer {event} />
+				<div>
+					{new Date(event.created_at * 1000).toLocaleString()}
+				</div>
 			</div>
 		</div>
-	</div>
-{/if}
+	{/if}
+</div>
