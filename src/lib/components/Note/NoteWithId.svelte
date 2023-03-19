c054ec5364e2ea809958b2eefba1596dcdd9537f
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-19 03:17:15 +0000

    feat: content内のノートとメンションの表示

---
+++ src/lib/components/Note/NoteWithId.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { notes } from '$lib/store/notes';
+	import Note from './Note.svelte';
+
+	export let id: string;
+
+	let event = notes.get(id);
+</script>
+
+{#if event}
+	<Note {event} />
+{/if}
