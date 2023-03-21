d72e33cdc1fd7ceaeba82cc19046ec66d2c014b1
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-21 03:09:37 +0000

    fix: リアクションの絵文字を絵文字ピッカーと同じに

---
+++ src/lib/components/Emoji.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import data from '@emoji-mart/data';
+	import { init, getEmojiDataFromNative } from 'emoji-mart';
+	import type { EmojiDate } from './EmojiPicker.svelte';
+
+	init({ data });
+
+	export let emoji: string;
+
+	let emojiData: EmojiDate;
+
+	getEmojiDataFromNative(emoji).then((e) => {
+		emojiData = e;
+	});
+</script>
+
+{#if emojiData}
+	<em-emoji id={emojiData.id} />
+{/if}
