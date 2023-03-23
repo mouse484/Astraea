bfedbd18363257090583a83ee92ae6a35322f295
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-23 10:27:56 +0000

    feat: 言語切替

--- src/lib/utils/i18n.ts
+++ src/lib/utils/i18n.ts
@@ -1,17 +1,31 @@
 import { browser } from '$app/environment';
-import { init, addMessages } from 'svelte-i18n';
+import { init, addMessages, locale } from 'svelte-i18n';
 
 import ja from '../locales/ja.json';
 import en from '../locales/en.json';
 
 const defaultLocale = 'ja';
 
+locale.subscribe((newLocale) => {
+	if (!browser) return;
+	if (newLocale) localStorage.setItem('locale', newLocale);
+});
+
 export const i18nInit = () => {
 	addMessages('ja', ja);
 	addMessages('en', en);
 
-	init({
-		fallbackLocale: defaultLocale,
-		initialLocale: browser ? window.navigator.language : defaultLocale
-	});
+	if (browser) {
+		const storedLocale = localStorage.getItem('locale');
+		const locale = storedLocale
+			? storedLocale
+			: window.navigator.language || defaultLocale;
+
+		init({
+			fallbackLocale: defaultLocale,
+			initialLocale: locale
+		});
+
+		localStorage.setItem('locale', locale);
+	}
 };
