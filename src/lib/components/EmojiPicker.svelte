737d9b55320fdcea448838965a7773ee5788efa2
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-23 13:48:28 +0000

    fix: emoji picker i18n root local

--- src/lib/components/EmojiPicker.svelte
+++ src/lib/components/EmojiPicker.svelte
@@ -5,8 +5,10 @@
 <script lang="ts">
 	import data from '@emoji-mart/data';
 	import { Picker } from 'emoji-mart';
-	import i18n from '@emoji-mart/data/i18n/ja.json';
+	import i18nJa from '@emoji-mart/data/i18n/ja.json';
+	import i18nEn from '@emoji-mart/data/i18n/en.json';
 	import { createEventDispatcher } from 'svelte';
+	import { locale } from 'svelte-i18n';
 
 	const dispatch = createEventDispatcher<{ onEmojiSelect: EmojiDate }>();
 
@@ -14,7 +16,11 @@
 		node: HTMLElement,
 		onEmojiSelect: (data: EmojiDate) => void
 	) => {
-		const picker = new Picker({ data, onEmojiSelect, i18n, locale: 'ja' });
+		const picker = new Picker({
+			data,
+			onEmojiSelect,
+			i18n: $locale === 'ja' ? i18nJa : i18nEn
+		});
 		node.appendChild(picker as unknown as Node);
 	};
 </script>
