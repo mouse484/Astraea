9bce445ebde60595b958bcd9e093d362df3d72c0
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-26 05:58:37 +0000

    feat: リプライの表示（仮）

--- src/lib/components/Note/Note.svelte
+++ src/lib/components/Note/Note.svelte
@@ -8,22 +8,23 @@
 	import NoteWithId from './NoteWithId.svelte';
 
 	export let event: Event;
+	export let isReply = false;
 
 	let isWarn = event.tags.find(([type]) => type === 'content-warning')?.[1];
 
-	const isReply = event.tags.reverse().find(([type, , , marker]) => {
+	const hasReply = event.tags.reverse().find(([type, , , marker]) => {
 		return type === 'e' && (!marker || marker === 'reply');
 	});
 </script>
 
 <div>
 	{#if event}
-		{#if isReply}
-			{@const [, id] = isReply}
-			<NoteWithId {id} />
+		{#if !isReply && hasReply}
+			{@const [, id] = hasReply}
+			<NoteWithId {id} {isReply} />
 		{/if}
 		<div
-			class="border p-2 rounded {isReply ? 'border-t-0' : ''}"
+			class="p-2 border rounded {isReply ? 'border-b-0 border-blue-800' : ''}"
 			on:dblclick={() => goto(`/note/${event.id}`)}
 		>
 			<Profile pubkey={event.pubkey} detail={false} />
@@ -36,7 +37,7 @@
 					<Content rawContent={event.content} tags={event.tags} />
 				{/if}
 			</div>
-			<div class="mt-4 flex justify-between">
+			<div class="flex justify-between mt-4">
 				<Footer {event} />
 				<div>
 					{new Date(event.created_at * 1000).toLocaleString()}
