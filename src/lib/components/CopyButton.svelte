376b1bbcea3364b6e6e3863051aa998548728ab6
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-27 01:27:53 +0000

    feat: profile menu
    close #272

---
+++ src/lib/components/CopyButton.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { onDestroy } from 'svelte';
+	import { _ } from 'svelte-i18n';
+	import { writable } from 'svelte/store';
+	import Button from './elements/Button.svelte';
+
+	export let content: string;
+	const copied = writable(false);
+
+	const onClick = async () => {
+		await navigator.clipboard.writeText(content);
+		copied.set(true);
+	};
+
+	const unsubscribe = copied.subscribe((value) => {
+		if (value) {
+			setTimeout(() => copied.set(false), 1000);
+		}
+	});
+
+	onDestroy(() => unsubscribe());
+</script>
+
+<Button on:click={onClick} disabled={$copied}>
+	{#if $copied}
+		{$_('general.copied')}
+	{:else}
+		<slot />
+	{/if}
+</Button>
