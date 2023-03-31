7223ee4e537faf3215ae5499929ba6cab5e6a830
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-03-31 05:07:18 +0000

    feat: i18n setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,13 @@
 	import { pwaInfo } from 'virtual:pwa-info';
 	import { onMount } from 'svelte';
 	import RegisterSw from '$lib/RegisterSW.svelte';
+	import { i18nInit } from '$lib/utils/i18n';
+	import Login from '$lib/components/Login.svelte';
 
 	let mounted = false;
+
+	i18nInit();
+
 	onMount(() => {
 		mounted = true;
 	});
@@ -16,6 +21,7 @@
 </svelte:head>
 
 <main>
+	<Login />
 	<slot />
 </main>
 
