6bac43f9b0ea9804dd569aeada447bce40685a92
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-04-05 13:15:08 +0000

    feat: 絵文字追加

--- src/lib/components/Note/Reactions.svelte
+++ src/lib/components/Note/Reactions.svelte
@@ -3,12 +3,17 @@
 	import { onDestroy } from 'svelte';
 	import { writable } from 'svelte/store';
 	import Emoji from '../elements/Emoji.svelte';
-	import type { Event } from 'nostr-tools';
+	import type { Event, UnsignedEvent } from 'nostr-tools';
 	import { pubkey } from '$lib/store/pubkey';
+	import { publishEvent, useRelays } from '$lib/nostr/pool';
+	import Icon from '@iconify/svelte';
+	import EmojiPicker from '../elements/EmojiPicker.svelte';
 
 	export let id: string;
 
 	const displayReactions = writable(new Map<string, Map<string, Event>>());
+
+	let isOpenEmojiPicker = false;
 
 	const unsubscribe = reactions.subscribe((v) => {
 		const react = v.get(id);
@@ -23,8 +28,22 @@
 		}
 	});
 
-	const hasOwnReaction = (items: Map<string, Event>) => {
+	const isHasOwnReaction = (items: Map<string, Event>) => {
 		return [...items.values()].findIndex((v) => v.pubkey === $pubkey) === -1 ? false : true;
+	};
+
+	const reactPublish = (content: string) => {
+		const unsignedEvent: UnsignedEvent = {
+			kind: 7,
+			created_at: Math.floor(Date.now() / 1000),
+			tags: [
+				['p', $pubkey, ''],
+				['e', id, '']
+			],
+			content: content,
+			pubkey: $pubkey
+		};
+		publishEvent(unsignedEvent, useRelays('write'));
 	};
 
 	onDestroy(() => {
@@ -32,16 +51,32 @@
 	});
 </script>
 
-<div class="flex max-w-max overflow-x-scroll">
+<div class="flex gap-2">
 	{#if $displayReactions}
 		{#each [...$displayReactions.entries()] as [reaction, items]}
-			<div
-				class="[&>.emoji]:h-4 flex gap-2 items-center rounded px-1
-        {hasOwnReaction(items) ? 'bg-blue-200' : ''}"
-			>
-				<Emoji emoji={reaction.replace('+', '❤')} />
-				<span>{items.size}</span>
-			</div>
+			{@const hasOwnReaction = isHasOwnReaction(items)}
+			<button on:click={() => reactPublish(reaction)} disabled={hasOwnReaction}>
+				<div
+					class="[&>.emoji]:h-4 flex gap-2 items-center rounded px-1
+        {hasOwnReaction ? 'bg-blue-200' : ''}"
+				>
+					<Emoji emoji={reaction.replace('+', '❤')} />
+					<span>{items.size}</span>
+				</div>
+			</button>
 		{/each}
 	{/if}
+	<div class="flex items-center relative">
+		<button on:click={() => (isOpenEmojiPicker = !isOpenEmojiPicker)}>
+			<Icon icon="material-symbols:add-reaction-outline" />
+		</button>
+		{#if isOpenEmojiPicker}
+			<EmojiPicker
+				on:onEmojiSelect={(event) => {
+					isOpenEmojiPicker = false;
+					reactPublish(event.detail.native);
+				}}
+			/>
+		{/if}
+	</div>
 </div>
