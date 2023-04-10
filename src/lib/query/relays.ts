7295ea9ce544eeba710307198869136d9d6fe3ec
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-04-10 12:46:50 +0000

    fix: デフォルトリレーは初期値ではなくエラーで動かないときに使う
    close #357

--- src/lib/query/relays.ts
+++ src/lib/query/relays.ts
@@ -14,13 +14,11 @@ export const relaysQuery = (pubkey: string) => {
 	return createQuery({
 		queryKey: ['relays', pubkey],
 		queryFn: async () => {
-			console.log('query');
 			const event = await getEvent(3, { authors: [pubkey] }, useRelays('read', defaultRelays));
 			const parsed = relayScheme.safeParse(JSON.parse(event.content));
-			if (!parsed.success) throw new Error('Cannot parse relay');
+			if (!parsed.success) return defaultRelays;
 			return parsed.data;
 		},
-		initialData: defaultRelays,
 		cacheTime: 1 * 60 * 60 * 1000,
 		staleTime: 1 * 60 * 60 * 1000
 	});
