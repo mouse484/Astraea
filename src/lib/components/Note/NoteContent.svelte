7632a12b02d34f39f020638cc50c6902caff7886
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-04-12 14:30:29 +0000

    feat: ハッシュタグをリンク化

--- src/lib/components/Note/NoteContent.svelte
+++ src/lib/components/Note/NoteContent.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import Markdown from '@magidoc/plugin-svelte-marked';
 	import NoteLink from './NoteLink.svelte';
+	import { marked } from 'marked';
+	import markedLinkifyIt from 'marked-linkify-it';
+
 	export let rawContent: string;
+
+	marked.use(
+		markedLinkifyIt({
+			'#': {
+				validate: /[^\s]+/,
+				normalize(match) {
+					match.url = `/search?q=${match.url.replace('#', '')}`;
+				}
+			}
+		})
+	);
 </script>
 
 <Markdown
