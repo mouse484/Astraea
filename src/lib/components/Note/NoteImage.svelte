b63d097face28d8251da7f1fe4630c0cab9fc357
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-04-12 04:27:04 +0000

    feat: ノート内の画像を見やすく

---
+++ src/lib/components/Note/NoteImage.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { clickoutside } from '@svelte-put/clickoutside';
+
+	export let src: string;
+	export let alt: string;
+
+	let popup = false;
+</script>
+
+<div class="mt-2">
+	<button
+		on:click={() => (popup = !popup)}
+		use:clickoutside
+		on:clickoutside={() => (popup = false)}
+	>
+		<img
+			{src}
+			{alt}
+			class={`border rounded ${popup ? 'fixed inset-0 m-auto max-h-full max-w-[80%]' : 'h-32'}`}
+		/>
+	</button>
+</div>
