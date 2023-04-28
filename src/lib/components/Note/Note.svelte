3e775d4352824f0ba40908a7ecc7d71eda68a399
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-04-28 03:55:45 +0000

    fix: nip36を適切に隠す
    close #414

--- src/lib/components/Note/Note.svelte
+++ src/lib/components/Note/Note.svelte
@@ -6,6 +6,7 @@
 	import NoteContent from './NoteContent.svelte';
 	import Reactions from './Reactions.svelte';
 	import NoteFooter from './NoteFooter.svelte';
+	import { _ } from 'svelte-i18n';
 
 	export let id: string;
 	export let isReply = false;
@@ -18,6 +19,10 @@
 	$: hasReply = event?.tags.reverse().find(([type, , , marker]) => {
 		return type === 'e' && (!marker || marker === 'reply');
 	});
+
+	let isNip36Visable = false;
+	// [type=content-warning,reason=string]
+	$: hasNip36Warn = event?.tags.find(([type]) => type === 'content-warning')?.[1];
 </script>
 
 {#if event}
@@ -40,7 +45,14 @@
 				<Profile pubkey={event.pubkey} />
 				<div class="text-sm">{new Date(event.created_at * 1000).toLocaleString()}</div>
 			</div>
-			<NoteContent rawContent={event.content} />
+			{#if hasNip36Warn && !isNip36Visable}
+				<button class="p-2 m-2 rounded border" on:click={() => (isNip36Visable = true)}>
+					<p>{$_('home.nip36.why')}</p>
+					<p>{$_('home.nip36.reason')}: <span class="font-bold">{hasNip36Warn}</span></p>
+				</button>
+			{:else}
+				<NoteContent rawContent={event.content} />
+			{/if}
 			<NoteFooter {id} />
 		</div>
 	</div>
