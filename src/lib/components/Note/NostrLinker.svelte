f806cd74146c28870dcb65b21917212744e4f63b
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-06 12:01:58 +0000

    feat: nevent

--- src/lib/components/Note/NostrLinker.svelte
+++ src/lib/components/Note/NostrLinker.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
+	import type { DecodeResult } from 'nostr-tools/lib/nip19';
 	import Mention from './Mention.svelte';
 	import Note from './Note.svelte';
 
-	export let token: { type: 'nostr'; ntype: string; text: string };
+	export let token: { type: 'nostr'; value: DecodeResult };
 
-	$: type = token.ntype;
-	$: id = token.text;
+	$: value = token.value;
 </script>
 
-{#if type === 'note'}
-	<Note {id} />
-{:else if type === 'npub'}
-	<Mention pubkey={id} />
+{#if value.type === 'note'}
+	<Note id={value.data} />
+{:else if value.type === 'nevent'}
+	<Note id={value.data.id} />
+{:else if value.type === 'npub'}
+	<Mention pubkey={value.data} />
 {:else}
-	<p class="text-pink-300">{type}:{id}</p>
+	<p class="text-pink-300">{value.type}:{value.data}</p>
 {/if}
