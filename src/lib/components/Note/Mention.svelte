68704bcd6384e537262ba049d58c7335e228c926
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-09 08:18:53 +0000

    fix: mention

--- src/lib/components/Note/Mention.svelte
+++ src/lib/components/Note/Mention.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { useRelays } from '$lib/nostr/relays';
 	import { profileQuery } from '$lib/query/profile';
-	import Link from '../elements/Link.svelte';
 
 	export let pubkey: string;
 
 	const query = profileQuery(pubkey, useRelays('read'));
 </script>
 
-<Link href="/profile/{pubkey}">@{$query.data.name}</Link>
+<a class="text-sky-400" href="/profile/{pubkey}">
+	@{$query.data?.name || pubkey.substring(0, 10)}
+</a>
