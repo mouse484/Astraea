d4545ced6731298d234a1edf4600aade157536d4
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-16 01:23:42 +0000

    feat: custom emoji (NIP-30)

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,14 +1,21 @@
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
 
-
 declare module 'marked-linkify-it' {
 	import type LinkifyIt from 'linkify-it';
-	import type { marked } from 'marked';
+	import type { marked, marked } from 'marked';
 
 	function linkify(
 		schemas?: { [key: string]: LinkifyIt.FullRule },
 		options?: LinkifyIt.Options
 	): marked.MarkedExtension;
 	export = linkify;
+}
+
+declare module 'marked-emoji' {
+	function markedEmoji(options: {
+		emojis: { [key: string]: string };
+		unicode?: boolean;
+	}): marked.MarkedExtension;
+	export { markedEmoji };
 }
