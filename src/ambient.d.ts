20fec3f1e3d2581c8b2c6fef1b1dfa4cf058993c
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-16 00:34:27 +0000

    fix: type defined

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,3 +1,7 @@
+/// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite-plugin-pwa/info" />
+
+
 declare module 'marked-linkify-it' {
 	import type LinkifyIt from 'linkify-it';
 	import type { marked } from 'marked';
