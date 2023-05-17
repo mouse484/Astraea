dad839b88126d40509914bb0d6e2755a11c620c4
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-17 14:20:05 +0000

    feat: new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,29 @@ const config = {
 		extend: {}
 	},
 
-	plugins: [require('daisyui')]
+	plugins: [require('daisyui')],
+
+	daisyui: {
+		themes: [
+			{
+				light: {
+					primary: '#83CCD2',
+					secondary: '#FFEB3B',
+					accent: '#11F4A5',
+					neutral: '#666666',
+					'base-100': '#FFFFFF'
+				},
+				dark: {
+					primary: '#2196F3',
+					secondary: '#D9FA6B',
+					accent: '#11F4A5',
+					neutral: '#333333',
+					'base-100': '#1F2C36',
+					'base-200': '#161F26',
+				}
+			}
+		]
+	}
 };
 
 module.exports = config;
