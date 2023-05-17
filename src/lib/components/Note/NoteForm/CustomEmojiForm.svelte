abf83fbf5f23073ffa9d083abc0a2c686105d1c1
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-17 15:01:18 +0000

    fix: emoji slect

--- src/lib/components/Note/NoteForm/CustomEmojiForm.svelte
+++ src/lib/components/Note/NoteForm/CustomEmojiForm.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { customEmojis, getCustomEmojis } from '$lib/store/customEmoji';
 	import { onMount } from 'svelte';
+	import { _ } from 'svelte-i18n';
 
 	export let emojis: [string, string, string][];
 
 	let selectedEmojis: string[] = [];
-	$: emojis;
 
 	$: emojis = selectedEmojis.map((code) => ['emoji', code, $customEmojis.get(code) || '']);
 
@@ -14,20 +14,26 @@
 	});
 </script>
 
-<div class="h-16 overflow-y-auto">
-	{#each [...$customEmojis.entries()] as [code, url] (code)}
-		<label class="label cursor-pointer">
-			<span class="label-text flex gap-2">
-				<img src={url} alt={code} class="h-4 w-4" />
-				{code}
-			</span>
-			<input
-				type="checkbox"
-				class="checkbox checkbox-primary ml-2"
-				bind:group={selectedEmojis}
-				value={code}
-			/>
-		</label>
-		<div class="divider" />
-	{/each}
-</div>
+{#if $customEmojis}
+	<div class="dropdown">
+		<button tabindex="-1" class="m-1 btn btn-primary">{$_('emoji.emoji')}</button>
+		<div tabindex="-1" class="w-max dropdown-content bg-base-100">
+			{#each [...$customEmojis.entries()] as [code, url] (code)}
+				<div class="w-full form-control">
+					<label class="input-group">
+						<span class="w-full">
+							<img src={url} alt={code} class="w-4 h-4" />
+							{code}
+						</span>
+						<input
+							type="checkbox"
+							class="checkbox checkbox-accent"
+							value={code}
+							bind:group={selectedEmojis}
+						/>
+					</label>
+				</div>
+			{/each}
+		</div>
+	</div>
+{/if}
