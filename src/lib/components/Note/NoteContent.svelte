190ff818d1115793a81b9952abd4964f9767898f
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-18 12:14:19 +0000

    feat: remove marked

--- src/lib/components/Note/NoteContent.svelte
+++ src/lib/components/Note/NoteContent.svelte
@@ -1,60 +1,14 @@
 <script lang="ts">
-	import Markdown from '@magidoc/plugin-svelte-marked';
-	import NoteLink from './NoteLink.svelte';
-	import { marked } from 'marked';
-	import markedLinkifyIt from 'marked-linkify-it';
-	import { markedEmoji } from 'marked-emoji';
-	import NostrLinker from './NostrLinker.svelte';
-	import { nip19, type Event } from 'nostr-tools';
-	import CustomEmoji from './CustomEmoji.svelte';
+	// import NoteLink from './NoteLink.svelte';
+	// import NostrLinker from './NostrLinker.svelte';
+	import type { Event } from 'nostr-tools';
+	// import CustomEmoji from './CustomEmoji.svelte';
 
 	export let event: Event;
 
-	const emojis = Object.fromEntries(
-		event.tags.filter(([key]) => key === 'emoji').map(([, code, url]) => [code, url])
-	);
-
-	const nostrLinkerMarkedExtention: marked.TokenizerExtension = {
-		name: 'nostr',
-		level: 'inline',
-		start(src) {
-			return src.match(/nostr:/)?.index;
-		},
-		tokenizer(src, tokens) {
-			const match = src.match(/^nostr:(?<id>[^ \s]+)/);
-			if (!match) return;
-			if (!match.groups?.id) return;
-			try {
-				const decoded = nip19.decode(match.groups.id);
-				return {
-					type: 'nostr',
-					raw: match[0],
-					value: decoded
-				};
-			} catch {}
-		}
-	};
-
-	marked.setOptions({ breaks: true, pedantic: true });
-	marked.use(markedEmoji({ emojis, unicode: false }));
-	marked.use(
-		markedLinkifyIt({
-			'#': {
-				validate: /[^\s]+/,
-				normalize(match) {
-					match.url = `/search?q=${match.url.replace('#', '')}`;
-				}
-			}
-		})
-	);
-	marked.use({ extensions: [nostrLinkerMarkedExtention] });
+	// const emojis = Object.fromEntries(
+	// 	event.tags.filter(([key]) => key === 'emoji').map(([, code, url]) => [code, url])
+	// );
 </script>
 
-<Markdown
-	source={event.content}
-	renderers={{
-		link: NoteLink,
-		nostr: NostrLinker,
-		emoji: CustomEmoji
-	}}
-/>
+{event.content}
