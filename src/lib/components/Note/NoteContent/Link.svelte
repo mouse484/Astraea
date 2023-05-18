323b094c158b9763150b3a2a3ac3e7acada4764b
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-18 13:24:07 +0000

    feat: markdown parser svelte-exmarkdown (unified)

---
+++ src/lib/components/Note/NoteContent/Link.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { HastNode } from 'svelte-exmarkdown/types';
+	import Children from 'svelte-exmarkdown/renderer/Children.svelte';
+
+	import NoteImage from '../NoteImage.svelte';
+	import Link from '$lib/components/elements/Link.svelte';
+
+	export let children: HastNode[];
+	export let properties: { href: string };
+
+	$: isImage = properties.href.match(/\.(jpe?g|png|gif|webp)(\??.*)$/);
+</script>
+
+{#if isImage}
+	<NoteImage src={properties.href} alt="" />
+{:else}
+	<Link href={properties.href}>
+		<Children {children} />
+	</Link>
+{/if}
