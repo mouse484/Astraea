a027be61b679524744d99d3e8646305dd4bf98fd
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-18 13:24:07 +0000

    feat: markdown parser svelte-exmarkdown (unified)

--- src/lib/components/Note/NoteContent.svelte
+++ src/lib/components/Note/NoteContent.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-	// import NoteLink from './NoteLink.svelte';
-	// import NostrLinker from './NostrLinker.svelte';
 	import type { Event } from 'nostr-tools';
+
+	import Markdown, { type Plugin } from 'svelte-exmarkdown';
+	import { gfmPlugin } from 'svelte-exmarkdown/gfm';
+	import remarkBreaks from 'remark-breaks';
+
+	import Link from './NoteContent/Link.svelte';
+
+	// import NostrLinker from './NostrLinker.svelte';
 	// import CustomEmoji from './CustomEmoji.svelte';
 
 	export let event: Event;
@@ -9,6 +15,8 @@
 	// const emojis = Object.fromEntries(
 	// 	event.tags.filter(([key]) => key === 'emoji').map(([, code, url]) => [code, url])
 	// );
+
+	const plugins: Plugin[] = [gfmPlugin, { remarkPlugin: remarkBreaks }, { renderer: { a: Link } }];
 </script>
 
-{event.content}
+<Markdown md={event.content} {plugins} />
