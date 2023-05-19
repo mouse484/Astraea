e5340c43995e54c9c3ccb5aa5510664829101031
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-05-19 12:31:15 +0000

    chore: remove marked

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,21 +1,2 @@
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
-
-declare module 'marked-linkify-it' {
-	import type LinkifyIt from 'linkify-it';
-	import type { marked, marked } from 'marked';
-
-	function linkify(
-		schemas?: { [key: string]: LinkifyIt.FullRule },
-		options?: LinkifyIt.Options
-	): marked.MarkedExtension;
-	export = linkify;
-}
-
-declare module 'marked-emoji' {
-	function markedEmoji(options: {
-		emojis: { [key: string]: string };
-		unicode?: boolean;
-	}): marked.MarkedExtension;
-	export { markedEmoji };
-}
