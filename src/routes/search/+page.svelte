89a0a19a56ceb64c4185ea75ca2cd9b2e1912700
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-06-22 11:43:22 +0000

    fix: search page render
    close #586

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,39 +1,32 @@
 <script lang="ts">
-	import { afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Heading from '$lib/components/elements/Heading.svelte';
 	import Section from '$lib/components/elements/Section.svelte';
 	import SearchForm from '$lib/components/Search/SearchForm.svelte';
-	// import SearchForm from '$lib/components/Search/SearchForm.svelte';
 	import TimeLine from '$lib/components/TimeLine/TimeLine.svelte';
 	import Title from '$lib/components/Title.svelte';
 	import { _ } from 'svelte-i18n';
-	import { writable } from 'svelte/store';
 
-	const searchQuery = writable('');
-
-	afterNavigate(() => {
-		searchQuery.set($page.url.searchParams.get('q') || '');
-	});
+	$: searchQuery = $page.url.searchParams.get('q');
 </script>
 
-<Title pageTitle="{$searchQuery} - {$_('search.search')}" />
+<Title pageTitle="{searchQuery} - {$_('search.search')}" />
 
 <div>
 	<Heading>{$_('search.search')}</Heading>
 
 	<Section className="h-full">
-		<h3 class="text-xl">{$searchQuery}</h3>
-		{#key $searchQuery}
-			{#if $searchQuery}
+		{#if searchQuery}
+			<h3 class="text-xl">{searchQuery}</h3>
+			{#key searchQuery}
 				<TimeLine
-					filter={{ limit: 30, search: $searchQuery }}
+					filter={{ limit: 30, search: searchQuery }}
 					relays={['wss://relay.nostr.band']}
 					authors="ALL"
 				/>
-			{:else}
-				<SearchForm />
-			{/if}
-		{/key}
+			{/key}
+		{:else}
+			<SearchForm />
+		{/if}
 	</Section>
 </div>
