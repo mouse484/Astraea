d55ddb9762ebf331644013bb5d01b176bd695c69
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-07-04 07:38:35 +0000

    feat: スマホ投稿画面

---
+++ src/routes/post/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import NoteForm from '$lib/components/Note/NoteForm/NoteForm.svelte';
+</script>
+
+<NoteForm
+	on:status={(event) => {
+		if (event.detail === 'success') {
+			goto('/');
+		}
+	}}
+/>
