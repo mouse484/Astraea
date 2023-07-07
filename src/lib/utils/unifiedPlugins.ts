1e2c2f4130ada8deb693ec12f8acda1c8f0ad89c
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-07-07 12:01:00 +0000

    fix: hashtag parser

--- src/lib/utils/unifiedPlugins.ts
+++ src/lib/utils/unifiedPlugins.ts
@@ -23,9 +23,8 @@ export const rehypeNostrContent: Plugin = (options: { emojis: { [keys: string]: 
 				}
 			],
 			[
-				/^#(\S+)/g,
+				/#(\S+)/g,
 				(raw, value) => {
-					console.log(raw)
 					return h('a', { href: `/search?q=${value}` }, raw);
 				}
 			]
