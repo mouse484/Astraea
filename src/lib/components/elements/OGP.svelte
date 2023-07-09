0db5b19d820e8d0881e3a7bd6ce962c7800c8b1b
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-07-09 02:29:31 +0000

    feat: ogp

---
+++ src/lib/components/elements/OGP.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { createQuery } from '@tanstack/svelte-query';
+
+	export let href: string;
+
+	const query = createQuery({
+		queryKey: ['ogp', href],
+		queryFn: async () => {
+			const response = await fetch(`/api/ogp?url=${encodeURIComponent(href)}`).catch((err) =>
+				console.log(err)
+			);
+			const result = await response?.json().catch((err) => console.log(err));
+			return result;
+		}
+	});
+	$: data = $query.data;
+</script>
+
+{#if data}
+	<div class="card card-compact w-96 bg-base-100 shadow-xl text-primary-content">
+		{#if data?.open_graph?.images?.[0]}
+			{@const image = data?.open_graph.images[0]}
+			<figure><img src={image.url} alt={image.alt} /></figure>
+		{/if}
+		<div class="card-body">
+			{#if data?.open_graph?.site_name}
+				<p class="flex items-center gap-4">
+					{#if data?.favicon}
+						<img src={data.favicon} alt={data?.title} class="h-4" />
+					{/if}
+					{data.open_graph.site_name}
+				</p>
+			{/if}
+			<h2 class="card-title">{data?.title}</h2>
+			{#if data?.description}
+				<p>{data.description}</p>
+			{/if}
+		</div>
+	</div>
+{/if}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,7 @@ const config = {
 			{
 				light: {
 					primary: '#83CCD2',
+					'primary-content': '#1F2C36',
 					secondary: '#FFEB3B',
 					accent: '#11F4A5',
 					neutral: '#666666',
@@ -22,11 +23,12 @@ const config = {
 				},
 				dark: {
 					primary: '#2196F3',
+					'primary-content': '#CED1D4',
 					secondary: '#D9FA6B',
 					accent: '#11F4A5',
 					neutral: '#333333',
 					'base-100': '#1F2C36',
-					'base-200': '#161F26',
+					'base-200': '#161F26'
 				}
 			}
 		]
