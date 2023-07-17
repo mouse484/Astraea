80d28fac5ffe49bbb3edabdf9a76a531ca3d49da
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-07-17 05:05:36 +0000

    fix: do not use analytics in vite dev mode

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,7 +1,13 @@
+<script lang="ts">
+	import { dev } from '$app/environment';
+</script>
+
 <svelte:head>
-	<script
-		async
-		src="/stats/script.js"
-		data-website-id="ab4e6af7-d2cb-4590-bd1b-8ad6878eee38"
-	></script>
+	{#if !dev}
+		<script
+			async
+			src="/stats/script.js"
+			data-website-id="ab4e6af7-d2cb-4590-bd1b-8ad6878eee38"
+		></script>
+	{/if}
 </svelte:head>
