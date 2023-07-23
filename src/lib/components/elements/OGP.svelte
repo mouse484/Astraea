6782b9832b567c39af80486051f0bcf67f33f846
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-07-23 02:09:41 +0000

    fix: ogp render update

--- src/lib/components/elements/OGP.svelte
+++ src/lib/components/elements/OGP.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { createQuery } from '@tanstack/svelte-query';
+	import type { Metadata } from 'unfurl.js/dist/types';
 
 	export let href: string;
+
+	let favionLoad = true;
 
 	const query = createQuery({
 		queryKey: ['ogp', href],
@@ -10,10 +13,11 @@
 				console.log(err)
 			);
 			const result = await response?.json().catch((err) => console.log(err));
-			return result;
+			return result as Metadata;
 		}
 	});
 	$: data = $query.data;
+	$: console.log(data);
 </script>
 
 {#if data}
@@ -25,16 +29,29 @@
 		<div class="card-body">
 			{#if data?.open_graph?.site_name}
 				<p class="flex items-center gap-4">
-					{#if data?.favicon}
-						<img src={data.favicon} alt={data?.title} class="h-4" />
+					{#if favionLoad}
+						{#if data?.favicon}
+							<img
+								class="h-4"
+								src={data.favicon}
+								alt={data?.title}
+								on:error={() => (favionLoad = false)}
+							/>
+						{/if}
 					{/if}
 					{data.open_graph.site_name}
 				</p>
 			{/if}
-			<h2 class="card-title">{data?.title}</h2>
-			{#if data?.description}
-				<p>{data.description}</p>
+			{#if data.title}
+				<h2 class="card-title">{data.title}</h2>
 			{/if}
+			<div class="max-h-40 overflow-clip">
+				{data.description || data.open_graph?.description || ''}
+			</div>
+
+			<p class="border-t border-dashed border-accent pt-1 text-right truncate ...">
+				{data?.open_graph?.url || href}
+			</p>
 		</div>
 	</div>
 {/if}
