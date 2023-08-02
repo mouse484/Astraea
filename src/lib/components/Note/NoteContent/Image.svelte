fcb8446899d34c3ce371ca86464fd19543b2f800
mouse_484 <38714187+mouse484@users.noreply.github.com>
2023-08-02 12:28:10 +0000

    fix: image modal

--- src/lib/components/Note/NoteContent/Image.svelte
+++ src/lib/components/Note/NoteContent/Image.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-	import { clickoutside } from '@svelte-put/clickoutside';
-
 	export let src: string;
 	export let alt: string;
 
-	let popup = false;
+	let modal: { showModal(): void };
 </script>
 
-<div class="mt-2">
+<button class="w-4/5 md:w-full md:max-w-md" on:click={() => modal.showModal()}>
+	<img {src} {alt} />
+</button>
+<dialog bind:this={modal} class="modal">
+	<form method="dialog" class="modal-box">
+		<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">X</button>
+		<img class="pt-4" {src} {alt} />
+	</form>
+</dialog>
+
+<!-- <div class="mt-2">
 	<button
 		on:click={() => (popup = !popup)}
 		use:clickoutside
@@ -19,4 +27,4 @@
 			class={`border rounded ${popup ? 'fixed inset-0 m-auto max-h-full max-w-[80%]' : 'h-32'}`}
 		/>
 	</button>
-</div>
+</div> -->
