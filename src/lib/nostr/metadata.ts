44dc6ee35cbeae1bb6d8b785475028b7f59f9b02
mouse484 <38714187+mouse484@users.noreply.github.com>
2025-06-13 23:25:16 +0900

    fix: streamline metadata fetching by utilizing metadataQuery function

--- src/lib/nostr/metadata.ts
+++ src/lib/nostr/metadata.ts
@@ -1,10 +1,37 @@
+import type { NPub } from 'nostr-tools/nip19'
+import { queryOptions } from '@tanstack/react-query'
+import { kinds, nip19, type SimplePool } from 'nostr-tools'
 import { z } from 'zod/v4'
 import { Nip01Kind0MetaDataSchema } from './nips/01'
 import { Nip05Kind0MetaDataSchema } from './nips/05'
 import { Nip24Kind0MetaDataSchema } from './nips/24'
 
-export const MetaDataSchema = z.object({
+const MetaDataSchema = z.object({
   ...Nip01Kind0MetaDataSchema.shape,
   ...Nip05Kind0MetaDataSchema.shape,
   ...Nip24Kind0MetaDataSchema.shape,
 }).partial().loose()
+
+export function metadataQuery(
+  pubkey: NPub,
+  { pool, relays }: { pool: SimplePool, relays: string[] },
+) {
+  const hex = nip19.decode(pubkey).data
+  return queryOptions({
+    queryKey: ['metadata', hex],
+    queryFn: async () => {
+      const event = await pool.get(relays, {
+        kinds: [kinds.Metadata],
+        authors: [hex],
+      })
+      if (!event) {
+        throw new Error(`Metadata not found for pubkey: ${pubkey}`)
+      }
+      const content = MetaDataSchema.parse(JSON.parse(event.content))
+      return {
+        event,
+        content,
+      }
+    },
+  })
+}
--- src/routes/(app)/(profile)/npub1{$id}.tsx
+++ src/routes/(app)/(profile)/npub1{$id}.tsx
@@ -1,31 +1,13 @@
 import type { NPub } from 'nostr-tools/nip19'
-import { kinds, nip19 } from 'nostr-tools'
 import Profile from '@/components/Profile'
-import { MetaDataSchema } from '@/lib/nostr/metadata'
+import { metadataQuery } from '@/lib/nostr/metadata'
 
 export const Route = createFileRoute({
   component: RouteComponent,
   loader: async ({ params: { id }, context: { queryClient, pool, relays } }) => {
     const pubkey = `npub1${id}` satisfies NPub
-    const hex = nip19.decode(pubkey).data
 
-    const event = await queryClient.fetchQuery({
-      queryKey: ['profile', hex],
-      queryFn: async () => {
-        return await pool.get(relays, {
-          kinds: [kinds.Metadata],
-          authors: [hex],
-        })
-      },
-    })
-
-    const result = MetaDataSchema.safeParse(JSON.parse(event?.content ?? '{}'))
-    const content = result.success ? result.data : undefined
-
-    return {
-      content,
-      event,
-    }
+    return queryClient.fetchQuery(metadataQuery(pubkey, { pool, relays }))
   },
 })
 
